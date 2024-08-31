4d2374d7cd71b4bcf7f3a93365c134d82306da8d
ChathuminiBandara <chathubandara500@gmail.com>
2024-08-31 12:47:39 +0530

    AppInit Way 2 : using way

--- src/main/java/lk/ijse/aad/springwebmvc/AppInit.java
+++ src/main/java/lk/ijse/aad/springwebmvc/AppInit.java
@@ -1,12 +1,20 @@
 package lk.ijse.aad.springwebmvc;
 
-import jakarta.servlet.ServletContext;
-import jakarta.servlet.ServletException;
-import org.springframework.web.WebApplicationInitializer;
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
 
-public class AppInit implements WebApplicationInitializer {
+public class AppInit extends AbstractAnnotationConfigDispatcherServletInitializer {
     @Override
-    public void onStartup(ServletContext servletContext) throws ServletException {
+    protected Class<?>[] getRootConfigClasses() {
+        return new Class[0];
+    }
 
+    @Override
+    protected Class<?>[] getServletConfigClasses() {
+        return new Class[0];
+    }
+
+    @Override
+    protected String[] getServletMappings() {
+        return new String[0];
     }
 }
