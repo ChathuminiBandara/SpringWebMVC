3dc7913187c1db84e5e7f124b0c89a97b66aa7a8
ChathuminiBandara <chathubandara500@gmail.com>
2024-08-31 12:44:21 +0530

    AppInit Way 1

---
+++ src/main/java/lk/ijse/aad/springwebmvc/AppInit.java
@@ -0,0 +1,12 @@
+package lk.ijse.aad.springwebmvc;
+
+import jakarta.servlet.ServletContext;
+import jakarta.servlet.ServletException;
+import org.springframework.web.WebApplicationInitializer;
+
+public class AppInit implements WebApplicationInitializer {
+    @Override
+    public void onStartup(ServletContext servletContext) throws ServletException {
+
+    }
+}
