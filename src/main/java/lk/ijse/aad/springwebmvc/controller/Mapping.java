205be311347dc2c3841c2cc238f131aa4d51eea1
ChathuminiBandara <chathubandara500@gmail.com>
2024-08-31 15:52:46 +0530

    Mapping spec with a space

---
+++ src/main/java/lk/ijse/aad/springwebmvc/controller/Mapping.java
@@ -0,0 +1,15 @@
+package lk.ijse.aad.springwebmvc.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("mapping")
+public class Mapping {
+    @GetMapping("hello")
+    public String helloMapping() {
+        return "Hello World";
+    }
+
+}
