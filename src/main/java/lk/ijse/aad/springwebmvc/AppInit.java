709bc05b2609cc5a8fcd8e0807be3c9a738ebd29
ChathuminiBandara <chathubandara500@gmail.com>
2024-08-31 14:23:42 +0530

    creating the context / Configurations & dispatcher

--- src/main/java/lk/ijse/aad/springwebmvc/AppInit.java
+++ src/main/java/lk/ijse/aad/springwebmvc/AppInit.java
@@ -1,20 +1,31 @@
 package lk.ijse.aad.springwebmvc;
 
+import lk.ijse.aad.springwebmvc.config.WebAppRootConfig;
 import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
 
 public class AppInit extends AbstractAnnotationConfigDispatcherServletInitializer {
+    //config 1
     @Override
     protected Class<?>[] getRootConfigClasses() {
-        return new Class[0];
+        return new Class[]{
+                WebAppRootConfig.class
+        };
     }
-
+    //config 2
     @Override
     protected Class<?>[] getServletConfigClasses() {
-        return new Class[0];
+        return new Class[]{
+                WebAppRootConfig.class
+        };
     }
 
+
+    //Dispatcher
     @Override
     protected String[] getServletMappings() {
-        return new String[0];
+        return new String[]{"/"};
     }
 }
+
+
+//complete eken pennanne Application Context eka
