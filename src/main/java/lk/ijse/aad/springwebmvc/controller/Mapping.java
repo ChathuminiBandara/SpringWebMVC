aac4ee5819efba9691ff4a80ed66d79763e15348
ChathuminiBandara <chathubandara500@gmail.com>
2024-08-31 16:01:19 +0530

    path segments adding : equals for wild card mapping

--- src/main/java/lk/ijse/aad/springwebmvc/controller/Mapping.java
+++ src/main/java/lk/ijse/aad/springwebmvc/controller/Mapping.java
@@ -1,6 +1,7 @@
 package lk.ijse.aad.springwebmvc.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,5 +12,11 @@ public class Mapping {
     public String helloMapping() {
         return "Hello World";
     }
+
+    @GetMapping("postget/*")
+    public String getHello(){
+        return "Hello World get";
+    }
+
 
 }
