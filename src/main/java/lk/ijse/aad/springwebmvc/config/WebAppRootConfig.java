8132464a97cd24ede074cb920bb2cd9ec9a35bae
ChathuminiBandara <chathubandara500@gmail.com>
2024-08-31 14:22:50 +0530

    Configurations for other layer

---
+++ src/main/java/lk/ijse/aad/springwebmvc/config/WebAppRootConfig.java
@@ -0,0 +1,9 @@
+package lk.ijse.aad.springwebmvc.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan(basePackages = "lk.ijse.aad.springwebmvc")
+public class WebAppRootConfig {
+}
